9a9cc77cf4442bce87c9050629f83d45a1d0c5b0
Bygaga63 <bygaga63@gmail.com>
2020-02-16 23:15:21 +0300

    add firestore initial value

--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -1,4 +1,5 @@
 import 'package:brew_crew/models/User.dart';
+import 'package:brew_crew/services/database_service.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
 class AuthService {
@@ -46,6 +47,9 @@ class AuthService {
       AuthResult result = await _auth.createUserWithEmailAndPassword(
           email: email, password: password);
       FirebaseUser user = result.user;
+
+      await DatabaseService(uid: user.uid)
+          .updateUserData('0', 'new crew member', 100);
       return _userFromFirebaseUser(user);
     } catch (e) {
       print(e.toString());
